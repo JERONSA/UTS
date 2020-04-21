316b2021ed8e8ae1c06958e2877e153874601866
jeniroan11061980@gmail.com <you@example.com>
2020-04-21 11:21:56 +0700

    Answer 1

---
+++ Answer_1.php
@@ -0,0 +1,9 @@
+<?php 
+$angka = 8;
+echo "Nilai Angka = $angka + 2 = ". ($angka + 2)."<br>";
+echo "Nilai Angka = $angka - 2 = ". ($angka - 2)."<br>";
+echo "Nilai Angka = $angka * 5 - 10 = ". ($angka * 5 - 10)."<br>";
+echo "Nilai Angka = $angka + 2 = ". ($angka + 2)."<br>";
+echo "Opeartor = $angka + 3 = ". ($angka + 3)."<br>";
+echo "Nilai Angka = $angka + 2 = ". ($angka + 2)."<br>";
+ ?>

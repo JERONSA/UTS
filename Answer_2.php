7831827f3ad02667d0ad2e65a4d38f0882d6007a
jeniroan11061980@gmail.com <you@example.com>
2020-04-21 11:24:36 +0700

    Answer 2

---
+++ Answer_2.php
@@ -0,0 +1,12 @@
+<?php 
+$nama = "Jeniroan Adolfina Isabela Sabuna";
+$umur = "40 Tahun";
+echo "Nama Saya = $nama ", "<br>";
+echo "Variabel Nama Saya" ;
+var_dump ($nama);
+echo "<br>";
+echo "Umur Saya = $umur";
+echo "<br>";
+echo "Variabel Umur Saya";
+var_dump($umur);
+ ?>
